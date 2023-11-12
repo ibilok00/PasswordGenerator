cf4868ad202c33ffe737594aa5ea4b00d6a1cb23
unknown <ivan.bilokapic97@gmail.com>
2023-11-13 00:47:31 +0100

    Add Password Generator project

---
+++ PasswordGenerator/PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.PasswordLengthLabel = new System.Windows.Forms.Label();
+            this.passwordLengthSlider = new System.Windows.Forms.TrackBar();
+            this.CopyPasswordButton = new System.Windows.Forms.Button();
+            this.generatePasswordButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordLengthSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Font = new System.Drawing.Font("Leelawadee UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(130, 36);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(530, 88);
+            this.passwordLabel.TabIndex = 0;
+            this.passwordLabel.Text = "label1";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PasswordLengthLabel
+            // 
+            this.PasswordLengthLabel.Font = new System.Drawing.Font("Leelawadee UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLengthLabel.Location = new System.Drawing.Point(130, 231);
+            this.PasswordLengthLabel.Name = "PasswordLengthLabel";
+            this.PasswordLengthLabel.Size = new System.Drawing.Size(530, 61);
+            this.PasswordLengthLabel.TabIndex = 1;
+            this.PasswordLengthLabel.Text = "Password Length: 5";
+            this.PasswordLengthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // passwordLengthSlider
+            // 
+            this.passwordLengthSlider.Location = new System.Drawing.Point(130, 186);
+            this.passwordLengthSlider.Name = "passwordLengthSlider";
+            this.passwordLengthSlider.Size = new System.Drawing.Size(530, 56);
+            this.passwordLengthSlider.TabIndex = 2;
+            this.passwordLengthSlider.Scroll += new System.EventHandler(this.passwordLengthSlider_Scroll);
+            // 
+            // CopyPasswordButton
+            // 
+            this.CopyPasswordButton.BackColor = System.Drawing.Color.White;
+            this.CopyPasswordButton.Font = new System.Drawing.Font("Leelawadee UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopyPasswordButton.Location = new System.Drawing.Point(420, 341);
+            this.CopyPasswordButton.Name = "CopyPasswordButton";
+            this.CopyPasswordButton.Size = new System.Drawing.Size(240, 57);
+            this.CopyPasswordButton.TabIndex = 3;
+            this.CopyPasswordButton.Text = "Copy Password";
+            this.CopyPasswordButton.UseVisualStyleBackColor = false;
+            this.CopyPasswordButton.Click += new System.EventHandler(this.CopyPasswordButton_Click);
+            // 
+            // generatePasswordButton
+            // 
+            this.generatePasswordButton.BackColor = System.Drawing.Color.White;
+            this.generatePasswordButton.Font = new System.Drawing.Font("Leelawadee UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generatePasswordButton.Location = new System.Drawing.Point(139, 341);
+            this.generatePasswordButton.Name = "generatePasswordButton";
+            this.generatePasswordButton.Size = new System.Drawing.Size(240, 57);
+            this.generatePasswordButton.TabIndex = 4;
+            this.generatePasswordButton.Text = "Generate";
+            this.generatePasswordButton.UseVisualStyleBackColor = false;
+            this.generatePasswordButton.Click += new System.EventHandler(this.generatePasswordButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.generatePasswordButton);
+            this.Controls.Add(this.CopyPasswordButton);
+            this.Controls.Add(this.passwordLengthSlider);
+            this.Controls.Add(this.PasswordLengthLabel);
+            this.Controls.Add(this.passwordLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.passwordLengthSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label PasswordLengthLabel;
+        private System.Windows.Forms.TrackBar passwordLengthSlider;
+        private System.Windows.Forms.Button CopyPasswordButton;
+        private System.Windows.Forms.Button generatePasswordButton;
+    }
+}
+
